02938cc42fb2b50a7f3f31242bcf7b777df5ded9
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-12-05 21:42:51 +0700

    menambahkan function EntrySiswaScreen pada file HalamanEntry.kt

--- app/src/main/java/com/example/datasiswa/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/datasiswa/ui/theme/Halaman/HalamanEntry.kt
@@ -1,9 +1,23 @@
 package com.example.datasiswa.ui.theme.Halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.datasiswa.R
+import com.example.datasiswa.model.EntryViewModel
+import com.example.datasiswa.model.PenyediaViewModel
 import com.example.datasiswa.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi{
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
+}
+
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
 }
