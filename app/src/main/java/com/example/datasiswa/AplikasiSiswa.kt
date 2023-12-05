db0891c9e0e08366651ed8d738c3f94b69c0daf0
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-12-05 21:03:39 +0700

    Menambahkan variable container dan function oncreate pada class AplikasiSiswa

--- app/src/main/java/com/example/datasiswa/AplikasiSiswa.kt
+++ app/src/main/java/com/example/datasiswa/AplikasiSiswa.kt
@@ -1,4 +1,17 @@
 package com.example.datasiswa
 
-class AplikasiSiswa {
+import android.app.Application
+import com.example.datasiswa.Repositori.ContainerApp
+import com.example.datasiswa.Repositori.ContainerDataApp
+
+class AplikasiSiswa : Application() {
+    /*
+    * AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+    * */
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
