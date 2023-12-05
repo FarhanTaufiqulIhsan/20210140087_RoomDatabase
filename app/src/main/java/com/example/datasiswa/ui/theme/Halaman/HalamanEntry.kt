04ad35fa6885f6ce2ca4761e87700e221bfd6f3b
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-12-05 21:31:07 +0700

    menambahkan object DestinasiEntry pada file HalamanEntry.kt

--- app/src/main/java/com/example/datasiswa/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/datasiswa/ui/theme/Halaman/HalamanEntry.kt
@@ -1,2 +1,9 @@
 package com.example.datasiswa.ui.theme.Halaman
 
+import com.example.datasiswa.R
+import com.example.datasiswa.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
