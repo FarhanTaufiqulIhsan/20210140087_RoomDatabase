8b30c9195f437210d53db2031c9747fcc8d0497b
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-12-05 22:22:29 +0700

    Menambahkan object DestinasiHome pada file HalamanHome.kt

--- app/src/main/java/com/example/datasiswa/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/datasiswa/ui/theme/Halaman/HalamanHome.kt
@@ -1,2 +1,9 @@
 package com.example.datasiswa.ui.theme.Halaman
 
+import com.example.datasiswa.R
+import com.example.datasiswa.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
